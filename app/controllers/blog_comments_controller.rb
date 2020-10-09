8f8a5a695a6b64d21a3545ebb7f011704d4acb39
Hideyo Gotoda <hidehide@HideyonoMacBook-Pro.local>
2020-10-09 21:21:26 +0900

    update

--- app/controllers/blog_comments_controller.rb
+++ app/controllers/blog_comments_controller.rb
@@ -1,10 +1,10 @@
 class BlogCommentsController < ApplicationController
   before_action :authenticate_user!
   def create
-    blog = Blog.find(params[:blog_id])
-    comment = current_user.blog_comments.new(blog_comment_params)
-    comment.blog_id = blog.id
-    comment.save
+    @blog = Blog.find(params[:blog_id])
+    @comment = current_user.blog_comments.new(blog_comment_params)
+    @comment.blog_id = @blog.id
+    @comment.save
     redirect_to request.referer
   end
   def destroy
